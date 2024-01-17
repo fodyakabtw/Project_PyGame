ea1609ac8abc7a7ae798d237305e0f361cf4ab69
Полина Болдова <pol1na.boldova@yandex.ru>
2024-01-17 03:38:54 +0300

    battle + main menu

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
 import pygame
 import sys
+import os
 
 pygame.init()
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y, enemy):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.columns = columns
+        self.enemy = enemy
+        self.attack = False
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        if self.attack and self.cur_frame == len(self.frames) - 1:
+            self.frames = []
+            if self.enemy == 'player':
+                self.cut_sheet(load_image('idle.png'), 12, 1)
+                self.rect = self.rect.move(160, 250)
+                self.cur_frame = 0
+                self.attack = False
+            else:
+                self.cut_sheet(load_image('slime_idle.png'), 5, 1)
+                self.rect = self.rect.move(480, 210)
+                self.cur_frame = 0
+                self.attack = False
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
 
 
 class ImageButton:  # Воспомогательный класс для загрузки кнопок на основное окно
@@ -92,7 +131,7 @@ def main_menu():
             if event.type == pygame.USEREVENT and event.button == play_button:
                 print('НАЖАТА КНОПКА "play_button"')
                 fade()
-                new_game()
+                battle()
 
             if event.type == pygame.USEREVENT and event.button == settings_button:
                 print('НАЖАТА КНОПКА "settings_button"')
@@ -297,6 +336,76 @@ def fade():
 
         pygame.display.flip()
         clock.tick(FPS)
+
+
+
+all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением {fullname} не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def battle():
+    size = width, height = 960, 600
+    screen1 = pygame.display.set_mode(size)
+    run = True
+    bow_img = load_image("bow.png")
+    numbs = [load_image("one.png"), load_image("two.png")]
+    bow_btn = ImageButton(80, 40, 80, 80, "",
+                          'data/bow.png')
+    sword_img = load_image("sword.png")
+    sword_btn = ImageButton(220, 40, 80, 80, "",
+                            'data/sword.png')
+    btns = [bow_btn, sword_btn]
+    background = load_image("battle_background.png")
+    clock = pygame.time.Clock()
+    player_idle = AnimatedSprite(load_image('idle.png'), 12, 1, 160, 250, 'player')
+    slime_idle = AnimatedSprite(load_image('slime_idle.png'), 5, 1, 480, 210, 'slime')
+    while run:
+        clock.tick(10)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_2 and not player_idle.attack:
+                player_idle.frames = []
+                player_idle.cut_sheet(load_image('attack_sword.png'), 8, 1)
+                player_idle.rect = player_idle.rect.move(160, 250)
+                player_idle.cur_frame = 0
+                player_idle.attack = True
+                slime_idle.frames = []
+                slime_idle.cut_sheet(load_image('slime_attack.png'), 9, 1)
+                slime_idle.rect = player_idle.rect.move(320, -35)
+                slime_idle.cur_frame = 0
+                slime_idle.attack = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_1 and not player_idle.attack:
+                player_idle.frames = []
+                player_idle.cut_sheet(load_image('attack_bow.png'), 5, 1)
+                player_idle.rect = player_idle.rect.move(160, 250)
+                player_idle.cur_frame = 0
+                player_idle.attack = True
+        screen1.blit(background, (0, 0))
+        for btn in btns:
+            btn.draw(screen1)
+        k = 0
+        for element in numbs:
+            screen1.blit(element, (100 + k, 120))
+            k += 140
+        all_sprites.draw(screen1)
+        all_sprites.update()
+        pygame.display.flip()
 
 
 main_menu()
